8051fc76a0badbfa76cc0a1d312d0aa03b563086
adamte <gustelad@student.gu.se>
2021-09-09 14:55:13 +0200

    routes for athlete

--- server/controllers/athletes.js
+++ server/controllers/athletes.js
@@ -5,12 +5,13 @@ var Athlete = require('../models/athlete');
 // Create a new athlete
 router.post('/api/athletes', function(req, res, next){
     var athlete = new Athlete(req.body);
+    athlete["username"] = "Testname";
     athlete.save(function(err, athlete){
         if (err) {return next(err);}
         res.status(201).json(athlete);
     })
 });
-/*
+
 // Return a list of all athletes
 router.get('/api/athletes', function(req, res, next) {
     Athlete.find(function(err, athletes) {
@@ -18,7 +19,7 @@ router.get('/api/athletes', function(req, res, next) {
         res.json({"athletes": athletes});
     });
 });
-*/
+
 
 // Return the athlete with the given ID
 router.get('/api/athletes/:id', function(req, res, next) {
@@ -31,23 +32,23 @@ router.get('/api/athletes/:id', function(req, res, next) {
         res.json(athlete);
     });
 });
-/*
+
 // Update the athlete with the given ID
-app.put('/camels/:id', function(req, res, next) {
+router.put('/api/athletes/:id/:newName', function(req, res, next) {
     var id = req.params.id;
-    Camel.findById(id, function(err, camel) {
+    var newName = req.params.newName;
+    Athlete.findById(id, function(err, athlete) {
         if (err) { return next(err); }
-        if (camel == null) {
-            return res.status(404).json({"message": "Camel not found"});
+        if (athlete == null) {
+            return res.status(404).json({"message": "Athlete not found"});
         }
-        camel.color = req.body.color;
-        camel.position = req.body.position;
-        camel.save();
-        res.json(camel);
+        athlete.username = newName;
+        athlete.save();
+        res.json(athlete);
     });
 });
-
-// Partially athlete the camel with the given ID
+/*
+// Partially update the athlete with the given ID
 app.patch('/camels/:id', function(req, res, next) {
     var id = req.params.id;
     Camel.findById(id, function(err, camel) {
@@ -61,21 +62,22 @@ app.patch('/camels/:id', function(req, res, next) {
         res.json(camel);
     });
 });
+*/
 
 // Delete the athlete with the given ID
-app.delete('/camels/:id', function(req, res, next) {
+router.delete('/api/athletes/:id', function(req, res, next) {
     var id = req.params.id;
-    Camel.findOneAndDelete({_id: id}, function(err, camel) {
+    Athlete.findOneAndDelete({_id: id}, function(err, athlete) {
         if (err) { return next(err); }
-        if (camel == null) {
-            return res.status(404).json({"message": "Camel not found"});
+        if (athlete == null) {
+            return res.status(404).json({"message": "Athlete not found"});
         }
-        res.json(camel);
+        res.json(athlete);
     });
 });
 
 // Error handler (must be registered last)
-app.use(function(err, req, res, next) {
+router.use(function(err, req, res, next) {
     console.error(err.stack);
     var err_res = {
         "message": err.message,
@@ -88,5 +90,5 @@ app.use(function(err, req, res, next) {
     res.json(err_res);
 });
 
-*/
+
 module.exports = router;
