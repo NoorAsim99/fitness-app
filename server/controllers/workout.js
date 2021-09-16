2314b5b3dd8ca2f613a6566668d2f82ddd6e8097
lnoklal <guslnoklal@student.gu.se>
2021-09-16 15:05:04 +0200

    Fixed issues with traininglogs

--- server/controllers/workout.js
+++ server/controllers/workout.js
@@ -95,7 +95,7 @@ router.delete('/api/workouts/:id/exercises/:exercise_id', function(req, res, nex
         if (workout == null) {
             return res.status(404).json({"message": "Workout not found"});
         }
-§
+        
         /* This will also delete the exercise from the database
         if(workout.exercises.includes(exerciseId)) {
             Exercise.findOneAndDelete({_id: exerciseId}, function(err, exercise) {
--- server/models/traininglog.js
+++ server/models/traininglog.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose');
-var traininglog = mongoose.traininglog;
+var Schema = mongoose.Schema;
 
-var traininglog = new traininglog({
-    title : { type : String},
-    date : { type : String},
+var traininglogSchema = new Schema({
+    entry : { type : String },
+    //date : { type : String }
 
 });
 
-module.exports = mongoose.model('traininglog', traininglog);
+module.exports = mongoose.model('traininglogs', traininglogSchema);
