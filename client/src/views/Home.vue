8bf9a31b56ba6571da665c7f83d8e2cd87b6ebb6
adamte <gustelad@student.gu.se>
2021-09-22 20:21:00 +0200

    Initial work on login/start pape

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,10 +1,32 @@
 <template>
   <div>
+    <!--
     <b-jumbotron header="DIT341 Frontend" lead="Welcome to your DIT341 Frontend Vue.js App">
       <b-button class="btn_message" variant="primary" v-on:click="getMessage()" >Get Message from Server</b-button>
       <p>Message from the server:<br/>
       {{ message }}</p>
     </b-jumbotron>
+    -->
+
+    <header>
+        <img class="logo" src="assets/Logo.png" alt="Fitness Tracker Logo">
+        <nav>
+          <ul class="nav__links">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">{{ message }}</a></li>
+          </ul>
+        </nav>
+        <a class="loginbtn" href="#"> <!-- <button>Login</button> --> <b-button class="lgn_btn" v-on:click="getMessage()" >Login</b-button> </a>
+      </header>
+
+      <div class="center-content">
+        <img id="previewImg" src="assets/Preview.png" alt="Features preview & signup">
+
+        <img id="loginImg" src="assets/LoginBg.png" alt="Blured colors">
+
+      </div>
+
   </div>
 </template>
 
@@ -21,8 +43,9 @@ export default {
   },
   methods: {
     getMessage() {
-      Api.get('/')
+      Api.get('/api/athletes')
         .then(response => {
+          console.log(response)
           this.message = response.data.message
         })
         .catch(error => {
