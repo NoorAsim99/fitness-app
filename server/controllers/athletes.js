e94efa11b85c85ce0e4a8190983cd4dae532bc34
adamte <gustelad@student.gu.se>
2021-09-29 14:30:45 +0000

    Get athlete now works with either id or username

--- server/controllers/athletes.js
+++ server/controllers/athletes.js
@@ -2,14 +2,15 @@ var express = require('express');
 var router = express.Router();
 var Athlete = require('../models/athlete');
 var Workout = require('../models/workout');
+var ObjectId = require('mongoose').Types.ObjectId;
 
 // Create a new athlete
-router.post('/api/athletes', function(req, res, next){
+router.post('/api/athletes', function (req, res, next) {
     var athlete = new Athlete(req.body);
-    athlete.save(function(err, athlete){
+    athlete.save(function (err, athlete) {
         if (err) {
-            if(err.code == 11000) { // Means an athlete with that username already exists
-                return res.status(400).json({"message" : "Username taken"});
+            if (err.code == 11000) { // Means an athlete with that username already exists
+                return res.status(400).json({ "message": "Username taken" });
             }
             else {
                 return next(err);
@@ -20,41 +21,45 @@ router.post('/api/athletes', function(req, res, next){
 });
 
 // Return a list of all athletes
-router.get('/api/athletes', function(req, res, next) {
-    Athlete.find(function(err, athletes) {
+router.get('/api/athletes', function (req, res, next) {
+    Athlete.find(function (err, athletes) {
         if (err) { return next(err); }
         if (athletes == null) {
-            res.status(404).json({"message": "Athletes not found"});
+            res.status(404).json({ "message": "Athletes not found" });
         }
-        res.json({"athletes": athletes});
+        res.json({ "athletes": athletes });
     });
 });
 
 
-// Return the athlete with the given ID
-router.get('/api/athletes/:id', function(req, res, next) {
+// Return the athlete with the given ID or given username
+router.get('/api/athletes/:id', function (req, res, next) {
     var id = req.params.id;
-    Athlete.findById(req.params.id, function(err, athlete) {
-        if (err) { return next(err); }
-        if (athlete == null) {
-            /* return res.status(404).json({"message": "Athlete not found"});
-            Instead of ending and returning a 404 check if the athlete was
-            searched for with the username instead of id */
-        } else {
-            res.json(athlete);
-        }
-    });
 
-    var name = req.params.id;
-    Athlete.find({ "username": name }, function(err, athlete) {
-        if (err) { return next(err); }
-        if (athlete == null || athlete.length === 0) {
-            res.status(404).json({"message": "Athlete not found"});
-        } else {
-            res.json(athlete);
-        }
-    });
+    if (ObjectId.isValid(id)) {
+        Athlete.findById(id, function (err, athlete) {
+            if (err) {
+                return next(err);
+            }
+            if (athlete == null) {
+                return res.status(404).json({"message": "Athlete not found"});
+            } else {
+                res.json(athlete);
+            }
+        });
+    } else {
+        var name = req.params.id;
+        Athlete.find({ "username": name }, function (err, athlete) {
+            if (err) { return next(err); }
+            if (athlete == null || athlete.length === 0) {
+                res.status(404).json({ "message": "Athlete not found" });
+            } else {
+                res.json(athlete);
+            }
+        });
+    }
 });
+
 
 /* This route has been baked into the one above and can probably be removed
 // Return the athlete with the given username -- HAS NO TEST
@@ -72,36 +77,36 @@ router.get('/api/athletes/:name', function(req, res, next) {
 */
 
 // Return the workouts in an athlete with the given ID
-router.get('/api/athletes/:id/workouts', function(req, res, next) {
+router.get('/api/athletes/:id/workouts', function (req, res, next) {
 
     var id = req.params.id;
 
-    Athlete.findById(req.params.id, function(err, athlete) {
+    Athlete.findById(req.params.id, function (err, athlete) {
         if (err) { return next(err); }
         if (athlete == null) {
-            return res.status(404).json({"message": "Athlete not found"});
+            return res.status(404).json({ "message": "Athlete not found" });
         }
 
         Workout.find({
             '_id': { $in: athlete.workouts }
-        }, function(err, foundWorkouts){
-             if (foundWorkouts.length != null) {
+        }, function (err, foundWorkouts) {
+            if (foundWorkouts.length != null) {
                 return res.status(200).json(foundWorkouts);
-             } else {
-                return res.status(404).json({"message": "Workouts not found"});
+            } else {
+                return res.status(404).json({ "message": "Workouts not found" });
             }
         });
     });
 });
 
 // Partially update the athlete with the given id by adding the id of a workout
-router.patch('/api/athletes/:id/:workoutId', function(req, res, next) {
+router.patch('/api/athletes/:id/:workoutId', function (req, res, next) {
     var id = req.params.id;
     var workId = req.params.workoutId;
-    Athlete.findById(id, function(err, athlete) {
+    Athlete.findById(id, function (err, athlete) {
         if (err) { return next(err); }
         if (athlete == null) {
-            return res.status(404).json({"message": "Athlete not found"});
+            return res.status(404).json({ "message": "Athlete not found" });
         }
         athlete.workouts.push(workId)
         athlete.save();
@@ -110,13 +115,13 @@ router.patch('/api/athletes/:id/:workoutId', function(req, res, next) {
 });
 
 // Update the athlete with the given ID
-router.put('/api/athletes/:id', function(req, res, next) {
+router.put('/api/athletes/:id', function (req, res, next) {
     var id = req.params.id;
     var newName = req.body.username;
-    Athlete.findById(id, function(err, athlete) {
+    Athlete.findById(id, function (err, athlete) {
         if (err) { return next(err); }
         if (athlete == null) {
-            return res.status(404).json({"message": "Athlete not found"});
+            return res.status(404).json({ "message": "Athlete not found" });
         }
         athlete.username = newName;
         athlete.save();
@@ -125,19 +130,19 @@ router.put('/api/athletes/:id', function(req, res, next) {
 });
 
 // Delete the athlete with the given ID
-router.delete('/api/athletes/:id', function(req, res, next) {
+router.delete('/api/athletes/:id', function (req, res, next) {
     var id = req.params.id;
-    Athlete.findOneAndDelete({_id: id}, function(err, athlete) {
+    Athlete.findOneAndDelete({ _id: id }, function (err, athlete) {
         if (err) { return next(err); }
         if (athlete == null) {
-            return res.status(404).json({"message": "Athlete not found"});
+            return res.status(404).json({ "message": "Athlete not found" });
         }
         res.json(athlete);
     });
 });
 
 // Error handler (must be registered last)
-router.use(function(err, req, res, next) {
+router.use(function (err, req, res, next) {
     console.error(err.stack);
     var err_res = {
         "message": err.message,
